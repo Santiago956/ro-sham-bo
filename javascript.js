974281e8c3dbd341919dda2bdd4007f9e0ec1387
Santiago956 <ppfsantiago@gmail.com>
2025-02-28 15:49:28 -0300

    Function now plays five rounds and declares winner

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
     let num = Math.random();
@@ -27,26 +26,43 @@ function getHumanChoice() {
 
 }
 
-function round (humanChoice, computerChoice) {
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let i = 1;
+    function round (humanChoice, computerChoice) {
 
-    let hChoice = getHumanChoice();
-    let cChoice = getComputerChoice();
-    
-    if (hChoice === cChoice) {
-        return 'Tie!'
-    }
-    else if (
-    (hChoice === 'rock' && cChoice === 'scissors') || 
-    (hChoice === 'scissors' && cChoice === 'paper') || 
-    (hChoice === 'paper' && cChoice === 'rock')) 
-    {
-        humanScore += 1
-        return `${hChoice} beats ${cChoice}, you win!`
+        let hChoice = getHumanChoice();
+        let cChoice = getComputerChoice();
         
+        if (hChoice === cChoice) {
+            console.log('Tie!')
+        }
+        else if (
+        (hChoice === 'rock' && cChoice === 'scissors') || 
+        (hChoice === 'scissors' && cChoice === 'paper') || 
+        (hChoice === 'paper' && cChoice === 'rock')) 
+        {
+            humanScore += 1
+            console.log(`${hChoice} beats ${cChoice}, you win!`);
+        }
+        else {
+            computerScore += 1
+            console.log(`${cChoice} beats ${hChoice}, you lose!`);
+        }
+    }
+    while (i < 5) {
+        i += 1;
+        round();
+    }
+    if (humanScore > computerScore) {
+        return 'Congratulations, you won the match!'
+    }
+    else if (humanScore < computerScore) {
+        return 'Too bad, you lost the match!'
     }
     else {
-        computerScore += 1
-        return `${cChoice} beats ${hChoice}, you lose!`
+        return "It's a tie!"
     }
 
 }
