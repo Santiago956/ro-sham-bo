a0db87f29370b217635a7f11192032f1b1624a21
Santiago956 <ppfsantiago@gmail.com>
2025-03-31 19:15:58 -0300

    UI added Works now with buttons instead of prompt's and shows the results

--- javascript.js
+++ javascript.js
@@ -1,68 +1,54 @@
-
-
 function getComputerChoice() {
-    let num = Math.random();
-    let sign;
-    if (num < 0.33) {
-        sign = 'rock';
-    }
-    else if (num >= 0.33 && num < 0.66) {
-        sign = 'paper';
-    }
-    else {
-        sign = 'scissors';
-    }
-    return sign;
+  let num = Math.random();
+  let sign;
+  if (num < 0.33) {
+    sign = "rock";
+  } else if (num >= 0.33 && num < 0.66) {
+    sign = "paper";
+  } else {
+    sign = "scissors";
+  }
+  return sign;
 }
 
-function getHumanChoice() {
-    let hSign = '';
-    while (!(hSign === 'rock' || hSign === 'paper' || hSign === 'scissors')) {
-        hSign = prompt('Rock, paper or scissors?');
-        hSign = hSign.toLowerCase();
-    }
-    return hSign;
-    
+function playGame(pchoice) {
+  let humanScore = 0;
+  let computerScore = 0;
+  let i = 1;
+  function round() {
+    let message = "";
+    let hChoice = pchoice.target.innerText.toLowerCase();
+    let cChoice = getComputerChoice();
 
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let i = 1;
-    function round (humanChoice, computerChoice) {
-
-        let hChoice = getHumanChoice();
-        let cChoice = getComputerChoice();
-        
-        if (hChoice === cChoice) {
-            console.log('Tie!')
-        }
-        else if (
-        (hChoice === 'rock' && cChoice === 'scissors') || 
-        (hChoice === 'scissors' && cChoice === 'paper') || 
-        (hChoice === 'paper' && cChoice === 'rock')) 
-        {
-            humanScore += 1
-            console.log(`${hChoice} beats ${cChoice}, you win!`);
-        }
-        else {
-            computerScore += 1
-            console.log(`${cChoice} beats ${hChoice}, you lose!`);
-        }
-    }
-    while (i < 5) {
-        i += 1;
-        round();
-    }
-    if (humanScore > computerScore) {
-        return 'Congratulations, you won the match!'
-    }
-    else if (humanScore < computerScore) {
-        return 'Too bad, you lost the match!'
-    }
-    else {
-        return "It's a tie!"
+    if (hChoice === cChoice) {
+      console.log("Tie!");
+      message = "Tie!";
+    } else if (
+      (hChoice === "rock" && cChoice === "scissors") ||
+      (hChoice === "scissors" && cChoice === "paper") ||
+      (hChoice === "paper" && cChoice === "rock")
+    ) {
+      humanScore += 1;
+      console.log(`${hChoice} beats ${cChoice}, you win!`);
+      message = `${hChoice} beats ${cChoice}, you win!`;
+    } else {
+      computerScore += 1;
+      console.log(`${cChoice} beats ${hChoice}, you lose!`);
+      message = `${cChoice} beats ${hChoice}, you lose!`;
     }
 
+    document.getElementById("result").textContent = message;
+  }
+  round();
+  /* while (i < 5) {
+    i += 1;
+    round();
+  }
+  if (humanScore > computerScore) {
+    return "Congratulations, you won the match!";
+  } else if (humanScore < computerScore) {
+    return "Too bad, you lost the match!";
+  } else {
+    return "It's a tie!";
+  } */
 }
